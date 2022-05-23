1dcfe4f1dc08ea28d258ec091a6e349efa9b2740
Farhan Khalid <farhan.khalid287@gmail.com>
2022-05-23 15:36:48 +0500

    added configs for prettier and eslit

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,7 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const DataTypes =  require("sequelize");
+const DataTypes = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
@@ -11,8 +11,8 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
@@ -23,8 +23,8 @@ db.sequelize = sequelize;
 db.user = require("./User.js")(sequelize, Sequelize, DataTypes);
 db.albums = require("./Albums.js")(sequelize, Sequelize, DataTypes);
 db.photos = require("./Photos.js")(sequelize, Sequelize, DataTypes);
-db.user.hasMany(db.albums, {foreignKey: 'user_id'})
-db.albums.belongsTo(db.user, {foreignKey: 'user_id'})
-db.albums.hasMany(db.photos, {foreignKey: 'album_id'})
-db.photos.belongsTo(db.albums, {foreignKey: 'album_id'})
+db.user.hasMany(db.albums, { foreignKey: "user_id" });
+db.albums.belongsTo(db.user, { foreignKey: "user_id" });
+db.albums.hasMany(db.photos, { foreignKey: "album_id" });
+db.photos.belongsTo(db.albums, { foreignKey: "album_id" });
 module.exports = db;
